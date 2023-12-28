6729c2d7fcb250c275dd6edc18315f27ef4c2273
tjahjoe <ntell634@gmail.com>
2023-12-29 06:49:56 +0700

    palindrome

---
+++ palndrome.java
@@ -0,0 +1,28 @@
+package logicjava;
+
+import java.util.Scanner;
+
+public class palndrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kata = input.next();
+        char tampung[] = new char[kata.length()];
+        boolean kondisi = false;
+        for (int i = 0; i < tampung.length; i++) {
+            tampung[i] = kata.charAt(i);
+        }
+        for (int i = 0; i < tampung.length / 2; i++) {
+            if (tampung[i] == tampung[(tampung.length - i) - 1]) {
+                kondisi = true;
+            } else {
+                kondisi = false;
+                break;
+            }
+        }
+        if (kondisi) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("bukan palindorome");
+        }
+    }
+}
