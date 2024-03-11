605502af89ce2fd173cca916d3ba5a9a1d692d3b
tjahjoe <ntell634@gmail.com>
2024-03-11 14:31:10 +0700

    minmax brute and divide

--- fibonacci.java
+++ fibonacci.java
@@ -3,11 +3,11 @@
 public class fibonacci {
     public static void main(String[] args) {
         int angka[] = new int[3];
-        angka[0] = 1;
+        angka[2] = 1;
         for (int i = 0; i < 5; i++) {
             angka[1] = angka[0];
             angka[0] = angka[2] + angka[1];
-            System.out.println(angka[2] + " + " + angka[1] + " : " + angka[0]);
+            System.out.println(angka[0]);
             angka[2] = angka[1];
         }
         fungsiRekursif(0, 1, 8);
---
+++ minimumMaksimum.java
@@ -0,0 +1,36 @@
+public class minimumMaksimum {
+    public static void main(String[] args) {
+        int [] a = {3, 55, 2, 5, 1000, 200};
+        System.out.println("Maksimum : " + devide(a[0], 1, a, a[0], a.length)[0]);
+        System.out.println("Minimum : " + devide(a[0], 1, a, a[0], a.length)[1]);
+
+        System.out.println("Maksimum : " + brute(a)[0]);
+        System.out.println("Minimum : " + brute(a)[1]);
+    }
+
+    public static int[] brute(int[] x){
+        int[] o = {x[0], x[0]};
+        for (int i = 1; i < x.length; i++) {
+            if (o[0] < x[i]) {
+                o[0] = x[i];
+            } else if (o[1] > x[i]) {
+                o[1] = x[i];
+            }
+        }  
+        return o;
+    }
+
+    public static int[] devide(int v ,int w, int[] x, int y, int z){
+        if (z <= 1 ) {
+            int o[] = {y, v};
+            return o;
+        }else{
+            if (y < x[w]) {
+                return devide(v, w+1, x, x[w], z-1);
+            } else if(v > x[w]){
+                return devide(x[w], w+1, x, y, z-1);
+            }
+            return devide(v, w+1, x, y, z-1);
+        }
+    }
+}
