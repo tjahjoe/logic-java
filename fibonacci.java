f7794f1d8ff6c566257a4254b7b6ea8d73421d72
tjahjoe <ntell634@gmail.com>
2024-03-11 07:43:47 +0700

    fibonacci fungsi rekursif

--- fibonacci.java
+++ fibonacci.java
@@ -1,4 +1,4 @@
-package logicjava;
+// package logicjava;
 
 public class fibonacci {
     public static void main(String[] args) {
@@ -9,7 +9,17 @@ public class fibonacci {
             angka[0] = angka[2] + angka[1];
             System.out.println(angka[2] + " + " + angka[1] + " : " + angka[0]);
             angka[2] = angka[1];
+        }
+        fungsiRekursif(0, 1, 8);
+    }
 
+    public static int fungsiRekursif(int x, int y, int z){
+        if (z < 1) {
+            return 0;
+        } else{
+            int a = x + y;
+            System.out.println(a);
+            return fungsiRekursif(a, x, z-1);
         }
     }
 }
