49a8f0accc9b336b47d19b13f21ea5e583f47cb9
tjahjoe <ntell634@gmail.com>
2024-04-20 10:49:42 +0700

    Menambahkan pilihan fitur

--- LatihanUTSSem2/main.java
+++ LatihanUTSSem2/main.java
@@ -1,17 +1,66 @@
 package LatihanUTSSem2;
 
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args) {
-       MethodMahasiswa mahasiswa = new MethodMahasiswa();
-       Mahasiswa data1 = new Mahasiswa("Wahyu Rizky Cahyana", "2341720006", "Teknologi Informasi");
-       
-       mahasiswa.tambahMahasiswa(data1);
-    //    System.out.println(mahasiswa.kurangMahasiswa());
-    //    System.out.println(mahasiswa.kurangMahasiswa());
-       System.out.println(mahasiswa.nextKurangMahasiswa());
-      //  mahasiswa.tampilMahasiswa();
-    //    mahasiswa.tambahMahasiswa(data1);
-    //    System.out.println(mahasiswa.kurangMahasiswa());
-       
-    }
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      MethodMahasiswa mahasiswa = new MethodMahasiswa();
+      boolean kondisi = true;
+      while (kondisi) {
+         System.out.println("1. Tambah mahasiswa");
+         System.out.println("2. Kurang mahasiswa");
+         System.out.println("3. Urutkan mahasiswa");
+         System.out.println("4. Tampil mahasiswa");
+         System.out.println("5. Cek mahasiswa");
+         switch (sc.nextLine()) {
+            case "1":
+               mahasiswa.inputMahasiswa(sc);
+               System.out.print("Ingin lanjut? (y/n) :");
+               if (sc.nextLine().equals("n"))
+                  kondisi = false;
+               break;
+            case "2":
+               System.out.println("Data yang di hapus : " + mahasiswa.kurangMahasiswa());
+               System.out.print("Ingin lanjut? (y/n) :");
+               if (sc.nextLine().equals("n"))
+                  kondisi = false;
+               break;
+            case "3":
+               MargeSort sort = new MargeSort();
+               sort.hasilMargeSort(mahasiswa.data);
+               System.out.print("Ingin lanjut? (y/n) :");
+               if (sc.nextLine().equals("n"))
+                  kondisi = false;
+               break;
+            case "4":
+               mahasiswa.tampilMahasiswa();
+               System.out.print("Ingin lanjut? (y/n) :");
+               if (sc.nextLine().equals("n"))
+                  kondisi = false;
+               break;
+            case "5":
+               pemilihanPencarian(sc, mahasiswa);
+               break;
+            default:
+               System.out.println("tidak valid");
+               break;
+         }
+      }
+   }
+
+   static void pemilihanPencarian(Scanner sc, MethodMahasiswa mahasiswa) {
+      System.out.println("1. Tanpa tumpukan");
+      System.out.println("2. Dengan tumpukan");
+      String pilih = sc.nextLine();
+      if (pilih.equals("1")) {
+         System.out.print("Masukkan Nama : ");
+         mahasiswa.cariMahasiswaBinary(sc.nextLine());
+      } else if (pilih.equals("2")) {
+         System.out.print("Masukkan Nama : ");
+         mahasiswa.cariMahasiswaSeq(sc.nextLine());
+      } else {
+         pemilihanPencarian(sc, mahasiswa);
+      }
+   }
 }
